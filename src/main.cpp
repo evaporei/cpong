5bb231f56e5b54d27db1318ef873189bc8663e2a
Eva Pace <eba.pachi@gmail.com>
2024-09-21 11:14:54 -0300

    draw scores (just 0)

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@
 #define HEIGHT 720
 
 #define TITLE "pong"
-#define FONT_SIZE 32
+#define SMALL_FONT_SIZE 32
+#define SCORE_FONT_SIZE 96
 
 #define PADDLE_WIDTH 15
 #define PADDLE_HEIGHT 60
@@ -17,6 +18,7 @@
 
 typedef struct Player {
     Vector2 pos;
+    unsigned int score;
 } Player;
 
 void handleInput(Player *p1, Player *p2) {
@@ -41,23 +43,27 @@ int main(void) {
 #endif
     InitWindow(WIDTH, HEIGHT, "game");
 
-    Font font = LoadFontEx("./font.ttf", FONT_SIZE, NULL, 0);
+    Font font = LoadFontEx("./font.ttf", SMALL_FONT_SIZE, NULL, 0);
 
-    Vector2 title_size = MeasureTextEx(font, TITLE, FONT_SIZE, 0);
+    Vector2 title_size = MeasureTextEx(font, TITLE, SMALL_FONT_SIZE, 0);
     Vector2 title_pos = Vector2{
         (WIDTH / 2.f) - title_size.x / 2.f,
         (60.f / 2) - title_size.y / 2.f
     };
 
-    Player p1 = { .pos = Vector2{30, 90} };
-    Player p2 = { .pos = Vector2{WIDTH - 30, HEIGHT - 150} };
+    Player p1 = { .pos = Vector2{15, 90}, .score = 0 };
+    Player p2 = { .pos = Vector2{WIDTH - 30, HEIGHT - 150}, .score = 0 };
 
     while (!WindowShouldClose()) {
         handleInput(&p1, &p2);
 
         BeginDrawing();
             ClearBackground(Color{ 40, 45, 52, 255 });
-            DrawTextEx(font, "pong", title_pos, FONT_SIZE, 0, LIGHTGRAY);
+            // DrawFPS(0, 0);
+            DrawTextEx(font, "pong", title_pos, SMALL_FONT_SIZE, 0, WHITE);
+
+            DrawTextEx(font, TextFormat("%d", p1.score), Vector2{WIDTH / 2.f - 150, HEIGHT / 3.f}, SCORE_FONT_SIZE, 0, WHITE);
+            DrawTextEx(font, TextFormat("%d", p2.score), Vector2{WIDTH / 2.f + 120, HEIGHT / 3.f}, SCORE_FONT_SIZE, 0, WHITE);
 
             // player 1 (left)
             DrawRectangle(p1.pos.x, p1.pos.y, PADDLE_WIDTH, PADDLE_HEIGHT, WHITE);
